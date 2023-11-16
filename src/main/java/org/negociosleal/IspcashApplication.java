dbe9af30d0f2fc94a2e38f97b46472f558d7e49e
Camilo Jesus Leal Patino <CJLEAL@bancolombia.com.co>
2023-11-16 08:16:51 -0500

    Commit Inicial se crea el proyecto

---
+++ src/main/java/org/negociosleal/IspcashApplication.java
@@ -0,0 +1,13 @@
+package org.negociosleal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IspcashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IspcashApplication.class, args);
+	}
+
+}
